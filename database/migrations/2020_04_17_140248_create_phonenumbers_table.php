ed61065ba69d5b31b4742c6c2f83117b2f29a9e9
LutenkoMikhail <lamprologus2011@gmail.com>
2020-04-17 17:19:03 +0300

    Add migrations

---
+++ database/migrations/2020_04_17_140248_create_phonenumbers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonenumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_numbers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('contact_id');
+            $table->string('phone_number', 100);
+            $table->timestamps();
+        });
+        Schema::table('phone_numbers', function (Blueprint $table) {
+            $table->foreign('contact_id')
+                ->references('id')
+                ->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phonenumbers');
+    }
+}
