96bdcae631a686c0904a892a3f5ecda5c8e2b350
LutenkoMikhail <lamprologus2011@gmail.com>
2020-04-23 20:10:27 +0300

    Refactoring templates

--- resources/views/phonenumber/create.blade.php
+++ resources/views/phonenumber/create.blade.php
@@ -2,7 +2,7 @@
 @section('title','Create Phone Number')
 @section('content')
 
-    <div class="thumbnail">
+    <div class="card">
         <div class="caption">
             <h3 class="text-center"> {{ __ ('Create Phone Number') }} </h3>
         </div>
@@ -13,7 +13,8 @@
             <div class="form-group row">
                 <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('phone number') }}</label>
                 <div class="col-md-6">
-                    <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror"
+                    <input id="phone_number" type="text"
+                           class="form-control @error('phone_number') is-invalid @enderror"
                            name="phone_number" value="{{ old('phone_number') }}" minlength="3" maxlength="50"
                            placeholder="phone number" required autocomplete="phone number" autofocus>
                     @error('phone_number')
@@ -23,9 +24,9 @@
             </div>
 
             <div class="text-center">
-                <button type="submit" class="btn btn-primary">Create Phone Number</button>
+                <button type="submit" class="btn btn-success">Create Phone Number</button>
                 <a href="{{ url()->previous() }}"
-                   class="btn btn-sm btn btn-success">{{ __('Back') }}</a>
+                   class="btn btn-primary">{{ __('Back') }}</a>
             </div>
         </form>
         <div class="col-md-12">
@@ -36,7 +37,4 @@
             @endif
         </div>
     </div>
-    </div>
-
-
 @endsection
